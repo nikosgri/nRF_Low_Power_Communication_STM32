3e6ec2d98b3110d460d18c74bdcefd11d88be738
Nick Grigoriadis <44061694+nikosgri@users.noreply.github.com>
2023-06-25 20:31:29 +0300

    Add files via upload

---
+++ Inc/cmsis/init_system.h
@@ -0,0 +1,16 @@
+/*
+ * init_system.h
+ *
+ *  Created on: Nov 7, 2021
+ *      Author: Medusa
+ */
+
+#ifndef INIT_SYSTEM_H_
+#define INIT_SYSTEM_H_
+
+#include "cmsis/stm32l0xx.h"
+
+
+void init_rcc(void);
+void clock_initialization(void);
+#endif /* INIT_SYSTEM_H_ */
